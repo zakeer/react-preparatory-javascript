667b0ca6c5f0586a13cd600046e60cd058906fb2
tejanayak12 <118418860+tejanayak12@users.noreply.github.com>
2023-03-17 12:50:14 +0530

    Changes-Added

--- 2-Javascript-Functions/1.High-order-return-function-arthamatic-operations.js
+++ 2-Javascript-Functions/1.High-order-return-function-arthamatic-operations.js
@@ -1,21 +1,21 @@
-function arthamaticopeartions(operations){
-    if(operations === '+'){
+function arithmeticOpeartions(operation){
+    if(operation === '+'){
         return function(a,b){
             return a+b;
         }
-    }else if(operations === '-'){
+    }else if(operation === '-'){
         return function(a,b){
             return a-b;
         }
-    }else if(operations === '*'){
+    }else if(operation === '*'){
         return function(a,b){
             return a*b;
         }
-    }else if(operations === '/'){
+    }else if(operation === '/'){
         return function(a,b){
             return a/b;
         }
-    }else if(operations === '%'){
+    }else if(operation === '%'){
         return function(a,b){
             return a%b;
         }
@@ -24,13 +24,13 @@ function arthamaticopeartions(operations){
     }
 };
 
-const addfunction = arthamaticopeartions('+');
-const multiplyfunction = arthamaticopeartions('*');
-const subractfunction = arthamaticopeartions('-');
-const divisionfunction = arthamaticopeartions('/');
-const modulefunction = arthamaticopeartions('%');
+const addfunction = arithmeticOpeartions('+');
+const multiplyfunction = arithmeticOpeartions('*');
+const subractfunction = arithmeticOpeartions('-');
+const divisionfunction = arithmeticOpeartions('/');
+const modulefunction = arithmeticOpeartions('%');
 
-const result1 = addfunction(85,960);//1045
+const result1 = addfunctionarithmeticOpeartions(85,960);//1045
 const result2 = multiplyfunction(852,963);//820476
 const result3 = subractfunction(852,963);//-111
 const result4 = divisionfunction(852,963);//0.8847352024922118
